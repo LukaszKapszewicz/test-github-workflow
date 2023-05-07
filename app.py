b6b79b495abba9173ecf13d53d55491ddb1fc785
vdi-terminal <vdi-terminal@EDU-WSB.local>
2023-05-07 16:00:26 +0200

    fixed2

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 """
 WSB Flask app.
 """
-
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     """
-    Returns a message
+    Returns a message.
     """
-
     return '<h1>Hello WSB! Greetings from Flask!</h1>'
+
 
 if __name__ == "__main__":
     app.run(debug=True)
